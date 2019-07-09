bd4ee8ca198064308c51b05a081dc73ddc96a227
Tianqiang Xu <2286455782@qq.com>
2019-07-09 21:41:46 +0800

    change 66

--- server/src/main/java/com/server/OrderController.java
+++ server/src/main/java/com/server/OrderController.java
@@ -77,6 +77,7 @@ public class OrderController {
             if(mutex.acquire(10, TimeUnit.SECONDS)){
                 ((LinkedList<CuratorFramework>) clients).push(client);
                 ((LinkedList<InterProcessMutex>) mutexes).push(mutex);
+                System.out.println("the id is : " + items.get(i).getId().intValue());
                 Commodity tmp = commodityRepository.findById(items.get(i).getId().intValue());
                 System.out.println("the tmp is " + tmp);
                 ((LinkedList<Commodity>) commodities).push(tmp);
