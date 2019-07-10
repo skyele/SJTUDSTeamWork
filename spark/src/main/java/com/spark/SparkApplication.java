e60df3f8b362f04dcbd1f236857d67c983de54c8
怜颐 吴 <wulianyi1998@sina.com>
2019-07-10 08:52:54 +0800

    merge mysql3

--- spark/src/main/java/com/spark/SparkApplication.java
+++ spark/src/main/java/com/spark/SparkApplication.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
