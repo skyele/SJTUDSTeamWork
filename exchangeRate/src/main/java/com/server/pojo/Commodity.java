bbdc3e1b35e2c4e23821dfba8ee7ea8c543b5b36
Tianqiang Xu <2286455782@qq.com>
2019-07-10 09:16:43 +0800

    change 83

--- exchangeRate/src/main/java/com/server/pojo/Commodity.java
+++ exchangeRate/src/main/java/com/server/pojo/Commodity.java
@@ -6,7 +6,6 @@ import javax.persistence.*;
 @Entity
 public class Commodity {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
     private String name;
     private Double price;
--- server/src/main/java/com/server/mysql/pojo/Commodity.java
+++ server/src/main/java/com/server/mysql/pojo/Commodity.java
@@ -5,7 +5,6 @@ import javax.persistence.*;
 @Entity
 public class Commodity {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
     private String name;
     private Double price;
