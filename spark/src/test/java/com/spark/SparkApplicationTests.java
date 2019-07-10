58d77780a40bfb78a101506436916c0ce611d300
wulianyi1998 <wulianyi1998@163.com>
2019-07-10 19:46:46 +0800

    try11

--- spark/src/test/java/com/spark/SparkApplicationTests.java
+++ spark/src/test/java/com/spark/SparkApplicationTests.java
@@ -1,19 +1,4 @@
 package com.spark;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SparkApplicationTests {
-	@Autowired
-	SparkService sparkService;
-	@Test
-	public void contextLoads() {
-		sparkService.foreachRDD();
-	}
-
 }
