6695316d62f95334ec99413108395deb0123dcad
怜颐 吴 <wulianyi1998@sina.com>
2019-07-12 10:30:08 +0800

    hibernate9

--- spark/src/main/java/com/spark/mysql/pojo/Hibernate4Utils.java
+++ spark/src/main/java/com/spark/mysql/pojo/Hibernate4Utils.java
@@ -2,36 +2,21 @@ package com.spark.mysql.pojo;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 import java.util.Objects;
 
 public class Hibernate4Utils {
 
-    static {
-        try {
-            Configuration conf = new Configuration().configure();
-            ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
-            sessionFactory = conf.buildSessionFactory(registry);
-        } catch (Throwable e) {
-            throw e;
-        }
-    }
-
-    public static final SessionFactory sessionFactory;
-
-    public static final ThreadLocal<Session> sessionThread = new ThreadLocal<>();
+    public static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
     /**
      * 获取当前线程安全Session
      */
     public static Session getCurrentSession() {
-        Session session = sessionThread.get();
+        Session session = sessionFactory.getCurrentSession();
         if (Objects.isNull(session)) {
             session = sessionFactory.openSession();
-            sessionThread.set(session);
         }
         return session;
     }
@@ -40,10 +25,9 @@ public class Hibernate4Utils {
      * 关闭Session
      */
     public static void closeCurrentSession() {
-        Session session = sessionThread.get();
+        Session session = sessionFactory.getCurrentSession();
         if (Objects.nonNull(session)) {
             session.close();
         }
-        sessionThread.set(null);
     }
 }
