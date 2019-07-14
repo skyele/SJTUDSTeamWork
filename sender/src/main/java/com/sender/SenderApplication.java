a70dc1ea8a26b1cb85dcbdb22cf37d521a24265b
Tianqiang Xu <2286455782@qq.com>
2019-07-14 23:03:23 +0800

    3 sender 3

--- sender/src/main/java/com/sender/SenderApplication.java
+++ sender/src/main/java/com/sender/SenderApplication.java
@@ -32,11 +32,11 @@ public class SenderApplication {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		SpringApplication.run(SenderApplication.class, args);
 		urlPort = args[3];
-		ItemGenerate itemGenerate = new ItemGenerate(Integer.parseInt(args[1]));
-		UserGenerate userGenerate = new UserGenerate(Integer.parseInt(args[0]));
 		for(int i = 0; i < 3; i++){
 			new Thread(){
 				public void run(){
+					ItemGenerate itemGenerate = new ItemGenerate(Integer.parseInt(args[1]));
+					UserGenerate userGenerate = new UserGenerate(Integer.parseInt(args[0]));
 					while(true){
 						try {
 							Thread.sleep(Integer.parseInt(args[2]));
